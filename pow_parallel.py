c03d57ebfab1a21efd4def3ad4ca464f431eaa33
obohusevych <oleksii.bohusevych@ratesetter.com>
2022-05-02 16:23:16 +0300

    Added the number of workers as input

--- pow_parallel.py
+++ pow_parallel.py
@@ -13,14 +13,14 @@ class Solver:
         print("Job Started")
         print("Workers %d" % len(self.workers))
 
-        text, level, nonces_per_worker = self.read_input()
+        text, level, number_of_workers, nonces_per_worker = self.read_input()
         nonce_start = 0
         start_time = time.time()
         found = False
         mapped = []
 
         while not found:
-            for i in range(0, len(self.workers)):
+            for i in range(0, number_of_workers):
                  mapped.append(self.workers[i].proof_of_work(
                     text, 
                     level, 
@@ -35,15 +35,15 @@ class Solver:
                     nonce = result
                     break
 
-            nonce_start += nonces_per_worker * len(self.workers)
+            nonce_start += nonces_per_worker * number_of_workers
             mapped = []
 
         elapsed_time = time.time() - start_time
         print("Wokrer N " + str(i) + " has found hash in " + str(elapsed_time) + " seconds.")
 
         hasht = Solver.compute_hash(text, nonce)
-        found_by_thread = (nonce // nonces_per_worker + 1) % len(self.workers)
-        output = Solver.get_proof_of_work_output(level, nonce, hasht, nonce, elapsed_time, found_by_thread, len(self.workers))
+        found_by_thread = (nonce // nonces_per_worker + 1) % number_of_workers
+        output = Solver.get_proof_of_work_output(level, nonce, hasht, nonce, elapsed_time, found_by_thread, number_of_workers)
         self.write_output(output)
 
         print("Job Finished")
@@ -52,9 +52,10 @@ class Solver:
         f = open(self.input_file_name, 'r')
         text = f.readline()
         level = int(f.readline())
+        workers = int(f.readline())
         nonces_per_worker = int(f.readline())
         f.close()
-        return text, level, nonces_per_worker
+        return text, level, workers, nonces_per_worker
 
     def write_output(self, output):
         f = open(self.output_file_name, 'w')
